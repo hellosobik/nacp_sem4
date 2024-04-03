b6ae0aa859a274a1a672682696c988f0774d3185
Sobik <hellosobik@gmail.com>
2024-04-03 10:38:07 +0530

    q3

---
+++ assignment/q3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+float eqn(float num){
+  float y_num;
+  y_num=num*exp(num)-cos(num);
+  // y_num=pow(num,2)-num-2;
+  return y_num;
+}
+int main()
+{
+  float x, y1, y2, y, x1=1, x2=3;
+  int counter=1;
+  while(1){
+    // y1=x1*exp(x1)-cos(x1);
+    // y2=x2*exp(x2)-cos(x2);
+    y1=eqn(x1);
+    y2=eqn(x2);
+
+    x=x1-y1*(x2-x1)/(y2-y1);//regula falsi formula
+    // y=x*exp(x)-cos(x);
+    y=eqn(x);
+    if ((y*y1)<0) {
+      x2=x;
+    }else {
+      x1=x;
+    }
+    printf("%d.\tx=%f, y=%f, x1=%f, x2=%f\n", counter,x,y,x1,x2);
+    if (y<0.00001 && y>-0.00001) {
+      printf("\nroot = %f \nno. of iteration= %d", x, counter);
+      break;
+    }
+    counter++;
+    // printf("%f\n", y);
+    // break;
+
+  }
+  return 0;
+}
