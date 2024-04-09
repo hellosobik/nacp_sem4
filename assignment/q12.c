39830f3ec60dbd7ce70e06dc76cdb8d5e83ae2fc
Sobik <hellosobik@gmail.com>
2024-04-09 08:12:54 +0530

    q12

--- assignment/q12.c
+++ assignment/q12.c
@@ -1,4 +1,62 @@
 #include <stdio.h>
+int fact(int x){
+  if (x==0) {
+    return 1;
+  }
+  int f=1;
+  for (int i = 1;i<=x;i++) {
+    f=f*i;
+  }
+  return f;
+}
+float p(float x, float xo, float h ){
+  float y = (x-xo)/h;
+  return y;
+}
+
+float xo[2](int size, float x_items[size], float x){
+  if (x<x_items[0]) {
+    return x_items[0];
+  }else if (x>x_items[-1]) {
+    return x_items[-1];
+  }else {
+    for (int i = 0;i<size-2;i++) {
+      if (x>x_items[i] && x<x_items[i+1]) {
+        float mid=(x_items[i+1]-x_items[i])/2+x_items[i];
+        if (x<mid) {
+          return x_items[i];
+        }else{
+          return x_items[i+1];
+        }
+      }
+    }
+  }
+}
+
+float pn(float p, int n){
+  if (n==0) {
+    return 1;
+  }
+  float p_n=1;
+  for (int i = 0;i<=n;i++) {
+    p_n*=p-i-1
+  }
+  return p_n;
+}
+
+float tn(){
+  float xo_num = xo(size, x_items[size], x);
+  float p_num = p(x, xo, h);
+  float y=0;
+  for (int i = 0;i<size;i++) {
+    float t1 = pn(p_num, i)/fact(i);
+    y+=
+
+  }
+  
+
+}
+
 int main(){
   int size;
   printf("please enter the no. of pairs: ");
